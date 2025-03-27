fbd10a2ec28e8591608df2443cb8b3aaf7ca25c8
Xasanov Ibrohim <xasanovdev.work@gmail.com>
2025-03-27 17:40:54 +0500

    feat: production:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,19 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     css: ['~/assets/css/main.css'],
     devtools: { enabled: true },
-    modules: ['nuxt-lucide-icons', '@pinia/nuxt'],
+    modules: ['nuxt-lucide-icons', '@pinia/nuxt', '@nuxtjs/supabase'],
+    supabase: {
+        redirect: false,
+        redirectOptions: {
+          login: '/login',
+          callback: '/confirm',
+          exclude: ['/']
+        }
+      },
+    runtimeConfig: {
+        public: {
+          supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+          supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
+        }
+      }
 });
