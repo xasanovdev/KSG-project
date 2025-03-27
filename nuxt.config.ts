215c0d65375e8d8ac344379002bda90f6c89e804
Xasanov Ibrohim <xasanovdev.work@gmail.com>
2025-03-27 17:46:02 +0500

    feat: switch of ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
+    ssr: false,
     css: ['~/assets/css/main.css'],
     devtools: { enabled: true },
     modules: ['nuxt-lucide-icons', '@pinia/nuxt'],
