46d880aed7875efcd34cabb3476558ab57e3e712
Xasanov Ibrohim <xasanovdev.work@gmail.com>
2025-03-28 10:12:31 +0500

    feat: redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
       },
     supabase: {
         url: process.env.NUXT_PUBLIC_SUPABASE_URL,
-        key: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
+        key: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
+        redirect: false
     }
 });
